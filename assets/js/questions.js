5f26b119127995a8b88215d61bad248045fac34f
Blaine Curren <blaine.curren@gmail.com>
2021-06-06 22:25:51 -0500

    initial files

---
+++ assets/js/questions.js
@@ -0,0 +1,38 @@
+let questions = [
+  {
+    numb: 1,
+    question: "What is the biggest animal in the world?",
+    answer: "Blue Whale",
+    options: ["Orca", "Blue Whale", "Giraffe", "Mouse"],
+  },
+  {
+    numb: 2,
+    question:
+      "In what franchise would you find the character Katniss Everdeen?",
+    answer: "The Hunger Games",
+    options: ["The Hunger Games", "Frozen", "Harry Potter", "Fast and Furious"],
+  },
+  {
+    numb: 3,
+    question: "What year was Heinz established?",
+    answer: "1869",
+    options: ["1944", "1776", "1844", "1869"],
+  },
+  {
+    numb: 4,
+    question: "To a single decimal point, many kilometers in a mile?",
+    answer: "1.6km",
+    options: [".47km", "3.3km", "1.6km", "1.9km"],
+  },
+  {
+    numb: 5,
+    question: "What was the most streamed show on Netflix in 2020?",
+    answer: "The Umbrella Academy season 2",
+    options: [
+      "Frontier",
+      "Kingdom",
+      "The Umbrella Academy season 2",
+      "Lucifer",
+    ],
+  },
+];
