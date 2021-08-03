4e4c704b78d5c41f1d1efb696435001000972db5
Blaine Curren <blaine.curren@gmail.com>
2021-08-03 00:23:46 -0500

    revised questions

--- assets/js/questions.js
+++ assets/js/questions.js
@@ -1,32 +1,32 @@
 var questions = [
   {
     numb: 1,
-    question: "What is the biggest animal in the world?",
+    question: "",
     answer: "Blue Whale",
     options: ["Orca", "Blue Whale", "Giraffe", "Mouse"],
   },
   {
     numb: 2,
     question:
-      "In what franchise would you find the character Katniss Everdeen?",
+      "",
     answer: "The Hunger Games",
     options: ["The Hunger Games", "Frozen", "Harry Potter", "Fast and Furious"],
   },
   {
     numb: 3,
-    question: "What year was Heinz established?",
+    question: "",
     answer: "1869",
     options: ["1944", "1776", "1844", "1869"],
   },
   {
     numb: 4,
-    question: "To a single decimal point, many kilometers in a mile?",
+    question: "",
     answer: "1.6km",
     options: [".47km", "3.3km", "1.6km", "1.9km"],
   },
   {
     numb: 5,
-    question: "What was the most streamed show on Netflix in 2020?",
+    question: "",
     answer: "The Umbrella Academy season 2",
     options: [
       "Frontier",
--- assets/js/script.js
+++ assets/js/script.js
@@ -17,3 +17,47 @@ var choiceA = document.getElementById("a");
 var choiceB = document.getElementById("b");
 var choiceC = document.getElementById("c");
 var choiceD = document.getElementById("d");
+
+var quizQuestions = [
+  {
+    question: "What is the biggest animal in the world?",
+    choiceA: "Orca",
+    choiceB: "Blue Whale",
+    choiceC: "Giraffe",
+    choiceD: "Mouse",
+    correctAnswer: "b",
+  },
+  {
+    question:
+      "In what franchise would you find the character Katniss Everdeen?",
+    choiceA: "The Hunger Games",
+    choiceB: "Frozen",
+    choiceC: "Harry Potter",
+    choiceD: "Fast and Furious",
+    correctAnswer: "a",
+  },
+  {
+    question: "What year was Heinz established?",
+    choiceA: "1944",
+    choiceB: "1776",
+    choiceC: "1844",
+    choiceD: "1869",
+    correctAnswer: "d",
+  },
+  {
+    question: "To a single decimal point, many kilometers in a mile?",
+    choiceA: ".47km",
+    choiceB: "3.3km",
+    choiceC: "1.6km",
+    choiceD: "1.9km",
+    correctAnswer: "c",
+  },
+  {
+    question: "What was the most streamed show on Netflix in 2020?",
+    choiceA: "Frontier",
+    choiceB: "The Umbrella Academy season 2",
+    choiceC: "Lucifer",
+    choiceD: "Kingdom",
+    correctAnswer: "b",
+  },
+];
